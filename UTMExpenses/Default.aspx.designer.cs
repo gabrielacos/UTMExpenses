c1478dd8bc4111a4cccdb67132158bf802aa7052
Beto <Beto@ALBERTOCOLO1999>
2018-02-28 14:09:47 -0400

    Css Added in every page

--- UTMExpenses/Default.aspx.designer.cs
+++ UTMExpenses/Default.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace UTMExpenses {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblMensaje;
+        
+        /// <summary>
+        /// Image3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image3;
     }
 }
